4d304eea1d07798646ecfbfd3b2dfc50e65be7f0
Allan Gadelha <allan.gadelha@fortics.com.br>
2021-02-15 21:31:50 -0300

    Service de regitro de usuário no banco e de task

--- index.js
+++ index.js
@@ -1,12 +1,16 @@
 const { ApolloServer } = require('apollo-server')
 const graphql = require('./src/graphql')
 const GitHubService = require('./src/services/GitHub.service')
+const TasksRegisterService = require('./src/services/TasksRegisterService')
+const UserRegisterService = require('./src/services/UserRegisterService')
 
 
 const server = new ApolloServer({
     ...graphql,
     dataSources: () => ({
-        GitHubService: GitHubService
+        GitHubService: GitHubService,
+        UserRegisterService: UserRegisterService,
+        TasksRegisterService: TasksRegisterService
     })
 })
 
--- src/graphql/modules/users/resolvers.js
+++ src/graphql/modules/users/resolvers.js
@@ -1,7 +1,24 @@
 module.exports = {
+    User: {
+        async tasks(user, _, { dataSources }) {
+            return await dataSources.TasksRegisterService.getTasks(user.id)
+        }
+    },
+
     Query: {
         async user(_, { login }, { dataSources }) {
-            return await dataSources.GitHubService.getUser(login)
+            const userFound = await dataSources.UserRegisterService.getUserByLogin(login)
+
+            if(userFound) return userFound
+
+            const {
+                login: loginGit, 
+                avatar_url
+            } = await dataSources.GitHubService.getUser(login)
+            return await dataSources.UserRegisterService.addUser({
+                login: loginGit, 
+                avatar_url
+            })
         }
     }
 }
